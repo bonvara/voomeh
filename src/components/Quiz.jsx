b54a0aa9f086dc9f9917f6ad994907ce4856d346
Bonvara <bonvara@pm.me>
2025-06-27 16:39:18 +0400

    refactor quiz into its own file

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,93 @@
+import { useState, useRef, useEffect } from "react";
+import Timer from "./Timer";
+import ConversionFormula from "./ConversionFormula";
+
+export function Quiz({ fromUnit, toUnit, conversionRate }) {
+  const inputRef = useRef(null);
+  const [feedback, setFeedback] = useState(null);
+  const [startingValue, setStartingValue] = useState(
+    () => Math.floor(Math.random() * 100) + 1
+  );
+  const correctAnswer = startingValue * conversionRate;
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleAnswer = () => {
+    let userAnswer = parseFloat(inputRef.current.value);
+    if (isNaN(userAnswer)) {
+      userAnswer = 0;
+    }
+    const difference = Math.abs(userAnswer - correctAnswer);
+    const maxDifference = Math.max(userAnswer, correctAnswer);
+    const accuracy = ((maxDifference - difference) / maxDifference) * 100;
+    setFeedback({
+      correct: correctAnswer.toFixed(2),
+      accuracy: Math.round(accuracy) + "%",
+    });
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      if (!feedback) {
+        handleAnswer();
+      } else {
+        handleNext();
+      }
+    }
+  };
+
+  const handleNext = () => {
+    setStartingValue(Math.floor(Math.random() * 100) + 1);
+    setFeedback(null);
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  };
+
+  return (
+    <div className="quiz-container">
+      <h1>Conversion Quiz</h1>
+      <h3>Train your intuitive converter</h3>
+      <div className="quiz-box">
+        <ConversionFormula
+          fromUnit={fromUnit}
+          toUnit={toUnit}
+          conversionRate={conversionRate}
+        />
+        <h2>
+          {startingValue} {fromUnit} is
+        </h2>
+        <h2>
+          <input
+            type="text"
+            ref={inputRef}
+            onKeyDown={handleKeyPress}
+            readOnly={!!feedback}
+          />{" "}
+          {toUnit}
+        </h2>
+        {!feedback && <Timer defaultTime={7} onTimeUp={handleAnswer} />}
+        {!feedback && (
+          <button className="show-answer-button" onClick={handleAnswer}>
+            Show Answer
+          </button>
+        )}
+        {feedback && (
+          <>
+            <div className="feedback">
+              <p>Answer:</p>
+              <p>
+                {feedback.correct} {toUnit}
+              </p>
+              <p>Accuracy: {feedback.accuracy}</p>
+            </div>
+            <button className="next-button" onClick={handleNext}>
+              Next
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
