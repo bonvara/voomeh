2e95c80c3198a3db67f69d02f0d5e64bf59bfdb9
Bonvara <bonvara@pm.me>
2025-06-27 17:07:21 +0400

    add length/currency dropdown

--- src/components/QuizContainer.jsx
+++ src/components/QuizContainer.jsx
@@ -1,8 +1,16 @@
+import "./QuizContainer.css";
+
 function QuizContainer({ children }) {
   return (
     <div className="quiz-container">
       <h1>Conversion Quiz</h1>
       <h3>Train your intuitive converter</h3>
+      <div className="conversion-type-selector">
+        <select>
+          <option value="currency">Currency</option>
+          <option value="length">Length</option>
+        </select>
+      </div>
       <div className="quiz-box">
         {children}
       </div>
